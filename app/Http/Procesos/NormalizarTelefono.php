042e9c563e3657ded637ad6936b82226c4dcf87c
Edwin Zapata <edwinrzc@gmail.com>
2022-03-31 16:31:22 -0300

    Fix query

--- app/Http/Procesos/NormalizarTelefono.php
+++ app/Http/Procesos/NormalizarTelefono.php
@@ -19,10 +19,10 @@ class NormalizarTelefono
 
     public function sql()
     {
-        return "SELECT normaliza_tel( \"$this->codPai\" , \"$this->telefono\" )as telefono,
-        normaliza_operador( \"$this->codPai\" , \"$this->telefono\" )as operador,
-        normaliza_localidad( \"$this->codPai\" , \"$this->telefono\" )as localidad,
-        normaliza_iscel( \"$this->codPai\" , \"$this->telefono\" )as es_movil";
+        return "SELECT normaliza_tel( '".$this->codPai."' , '".$this->telefono."' )as telefono,
+        normaliza_operador( '".$this->codPai."' , '".$this->telefono."' )as operador,
+        normaliza_localidad( '".$this->codPai."' , '".$this->telefono."' )as localidad,
+        normaliza_iscel( '".$this->codPai."' , '".$this->telefono."' )as es_movil";
     }
 
 
